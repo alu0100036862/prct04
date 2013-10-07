6fd7cc3f86494cd78103a497ec2ab653712ac7d2
Dailos Herrera Bencomo <alu0100036862@etsii.ull.es>
2013-10-07 19:09:28 +0100

    conflicto de prueba desde master

--- prct04.rb
+++ prct04.rb
@@ -37,6 +37,13 @@ end
 
 #################################################################
 
+#Este es el error que me dio:
+#error: failed to push some refs to 'git@github.com:alu0100036862/prct04.git'
+#To prevent you from losing history, non-fast-forward updates were rejected
+#Merge the remote changes (e.g. 'git pull') before pushing again.  See the
+#'Note about fast-forwards' section of 'git push --help' for details.
+
+
 puts "Prueba: sumar y multiplicar dos matrices a y b"
 puts "=============================================="
 
